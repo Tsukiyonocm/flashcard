e3b91afb10c15b1566a9be7710a1dc6c4f3ebc7a
Tsukiyonocm <tsukiyonocm@yahoo.com>
2018-10-09 11:25:22 -0400

    completed basic functionality, some css work on buttons and inputs

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,6 @@ let chosen = [];
 // Selected Question
 let qChosen = [];
 
-// Array of Answers
-const answers = [];
-
 // Toggles if a new question is selected
 let toggle = 0;
 
@@ -20,36 +17,55 @@ let answer = document.querySelector(".input_answer-input");
 let submit = document.querySelector(".input_submit-button");
 let display = document.querySelector(".input_display");
 let card = document.querySelector(".card_container");
+let guessInput = document.querySelector(".guess_input");
+let submitGuess = document.querySelector(".submitGuess");
+let nextQuestion = document.querySelector(".nextQuestion");
 
 // Select P quiz card values
 let cardQuestion = document.querySelector(".quiz_question");
 let cardAnswer = document.querySelector(".quiz_answer");
 
-
 // Event Listener on Submit Button for Display Items Idividually
 submit.addEventListener("click", function() {
-    questions.push(question.value);
-    answers.push(answer.value);
+    createObj();
 
     let trashCan = createDisplayItem();
-    trashCan.addEventListener("click", function(){
-        // confirm("Are you sure you want to delete this?") && 
+    trashCan.addEventListener("click", function() {
         this.parentNode.remove();
-    })
+    });
     inputReset();
     toggle = questions.length;
 });
 
+// Event Listener to test if guess is correct
+submitGuess.addEventListener("click", function() {
+    if (guessInput.value.toLowerCase() === qChosen.answer.toLowerCase()) {
+        card.classList.toggle("flip");
+        submitGuess.disabled = true;
+    } else {
+        console.log("wrong or not working");
+    }
+});
 
+nextQuestion.addEventListener("click", function() {
+    card.classList.toggle("flip");
+    submitGuess.disabled = false;
+    setTimeout(generateCard, 1000);
+});
+
+// Create The object for inclusion to array
+function createObj() {
+    let obj = {};
+    obj.question = question.value;
+    obj.answer = answer.value;
+    questions.push(obj);
+}
 
 // Resets inputs to blank after submit
 function inputReset() {
     question.value = "";
     answer.value = "";
 }
-
-
-
 
 // Creates Each Display Item
 function createDisplayItem() {
@@ -81,28 +97,29 @@ function createDisplayItem() {
     return newUl.appendChild(trashCan);
 }
 
-
 //Generating Card Information per question
-function generateCard(){
+function generateCard() {
     random();
-    if (toggle < 0){
+    if (toggle < 0) {
         cardQuestion.textContent = "There are no more questions left";
-    }
-    else {
-        cardQuestion.textContent = qChosen;
+        cardAnswer.textContent = "There are no more questions left";
+    } else {
+        cardQuestion.textContent = qChosen.question;
+        cardAnswer.textContent = qChosen.answer;
     }
 }
 
-
 // Choses a random value for the selection set
-function random() {  
-    if(questions.length === 0){
+function random() {
+    if (questions.length === 0) {
         toggle = -1;
-    }
-    else {
+    } else {
         let num = Math.floor(Math.random() * questions.length);
         chosen = questions.splice(num, 1).concat(chosen);
         qChosen = chosen[0];
     }
 }
 
+// Notes
+// I need to create a function that upon submit of a guess, checks its value against the answer textContent.
+// I will likely need to make the text lowercase for the check to just make sure that they match exactly and that a capital letter wont create a false when its true.
